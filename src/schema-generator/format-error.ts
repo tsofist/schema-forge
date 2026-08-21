690c5792073ec067ebd095ffd1bb56b88b701df0
Andrew Berdnikov <tsofistgudmen@gmail.com>
2026-08-21 19:26:24 -0300

    fix(deps): bump deps

--- src/schema-generator/format-error.ts
+++ src/schema-generator/format-error.ts
@@ -3,7 +3,7 @@ import { formatDiagnostics } from 'typescript';
 
 export function formatForgeSchemaError(error: Error, dir = '') {
     if (error instanceof BaseError) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         return formatDiagnostics(error.diagnostic.relatedInformation || [], {
             getCanonicalFileName: (fileName) => fileName,
             getCurrentDirectory: () => dir,
